ae1e89b7c876d10b7333489ea383dc63052b31a6
Carl Centola <carl.centola@gmail.com>
2018-10-31 17:04:55 -0400

    added error handling

--- company/__init__.py
+++ company/__init__.py
@@ -1,3 +1,8 @@
 from flask import Flask
 
 app = Flask(__name__)
+
+from company.core.views import core
+from company.error_pages.handlers import error_pages
+app.register_blueprint(core)
+app.register_blueprint(error_pages)
